c8e06706f802164498cd965fdf48780d6fa62474
Felix Sandvik <siantyx@gmail.com>
2014-09-12 14:05:12 +0200

    Lejos lib inlagt. Bytt namn på huvud fil. Gjort ett package. Gjort så roboten visar tet och väntar på input.

---
+++ legorobot/src/robot/Robot.java
@@ -0,0 +1,10 @@
+package robot;
+import lejos.nxt.*;
+
+public class Robot {
+
+	public static void main(String[] args) {
+		System.out.println("Test.\nPress any button to exit.");
+		Button.waitForAnyPress();
+	}
+}
