ec3d7a08fe488c79c1097f2ab334eeda441c3b68
Felix Sandvik <siantyx@gmail.com>
2014-09-12 15:35:06 +0200

    frambaksnurra

--- legorobot/src/robot/Robot.java
+++ legorobot/src/robot/Robot.java
@@ -3,13 +3,27 @@ import lejos.nxt.*;
 
 public class Robot {
 
-	public static void main(String[] args) {
-		System.out.println("Test.\nPress any button to go forward.");
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("Press any button to go run test.");
 		Button.waitForAnyPress();
 		NXTRegulatedMotor engineA = new NXTRegulatedMotor(MotorPort.A);
 		NXTRegulatedMotor engineC = new NXTRegulatedMotor(MotorPort.C);
 		engineA.forward();
 		engineC.forward();
+		Thread.sleep(2000);
+		engineA.stop();
+		engineC.stop();
+		Thread.sleep(1000);
+		engineA.backward();
+		engineC.backward();
+		Thread.sleep(2000);
+		engineA.stop();
+		engineC.stop();
+		Thread.sleep(1000);
+		engineA.forward();
+		Thread.sleep(2000);
+		engineA.stop();
+		System.out.println("Test complete.\nPress any button to exit program.");
 		Button.waitForAnyPress();
 		
 		//ngfnfhgf
