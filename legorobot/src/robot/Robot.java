49ac4d668db1c9458d1aee69616c794d120b8597
Felix Sandvik <siantyx@gmail.com>
2014-09-12 15:04:15 +0200

    Nu åker den frammåt

--- legorobot/src/robot/Robot.java
+++ legorobot/src/robot/Robot.java
@@ -4,7 +4,12 @@ import lejos.nxt.*;
 public class Robot {
 
 	public static void main(String[] args) {
-		System.out.println("Test.\nPress any button to exit.");
+		System.out.println("Test.\nPress any button to go forward.");
+		Button.waitForAnyPress();
+		NXTRegulatedMotor engineA = new NXTRegulatedMotor(MotorPort.A);
+		NXTRegulatedMotor engineC = new NXTRegulatedMotor(MotorPort.C);
+		engineA.forward();
+		engineC.forward();
 		Button.waitForAnyPress();
 	}
 }
